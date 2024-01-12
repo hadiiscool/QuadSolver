68767b3bd47829817c43de6bf9bd83fe0b2d46a5
Hadi Ammar <hammar24@students.polytechnic.org>
2024-01-12 14:29:33 -0800

    mm

--- QuadraticSolver.java
+++ QuadraticSolver.java
@@ -1,7 +1,7 @@
 public class QuadraticSolver{
 public static void main(String[] args) {
 
-    System.out.println("welcoe to the Quad Solver");
+    System.out.println("welcome to the Quad Solver");
 
 }
 }
